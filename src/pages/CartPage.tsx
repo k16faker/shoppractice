92e8b1b54718e8e2b194e41bcd41be95d2c94179
ParkSeongJun <sjyj98@naver.com>
2024-01-05 15:41:10 +0900

    설명 추가

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,27 +1,39 @@
 import { useAppSelector } from "../store/store";
 import { useAppDispatch } from "../store/store"
 import { removeCart } from "../store/cartSlice";
+import { useState } from "react";
 
 function CartPage(): JSX.Element {
   const carts = useAppSelector((state) => state.cart.items);
   const dispatch = useAppDispatch();
+  const [isExist, setIsExist] = useState<boolean>(false);
+
+  if(carts.length === 0) {
+    return (
+      <div className="border rounded-xl w-6/12 h-36 mx-auto mt-6 border-slate-800 items-center">
+        <p className="text-center mt-10">There is no item in your cart</p>
+      </div>
+    )
+  }
 
   return (
-    <div>
-      <ul className="w-4/12 mx-auto grid grid-cols-2 leading-6">
+    <div className="border rounded-xl w-6/12 mx-auto mt-6 border-slate-800">
+      <ul className="mx-auto leading-6 mt-6">
         {carts.map((cart) => (
-          <li key={cart.name} className="border rounded-xl text-center w-4/12 mx-auto">
+          <li key={cart.name} className="flex border rounded-xl text-center w-6/12 h-40 px-6 mx-auto items-center justify-between">
             <h3>{cart.name}</h3>
-            <p>{cart.price}$</p>
-            <p>Quantity : {cart.quantity}</p>
-            <button onClick={()=> dispatch(removeCart({name:cart.name}))}>Remove</button>
+            <div className="flex w-6/12 justify-between">
+              <div className="mr-4">
+                <p>{cart.price}$</p>
+                <p>Quantity : {cart.quantity}</p>
+                <p>Price : {cart.price * cart.quantity}$</p>
+              </div>
+              <button className="bg-red-500 h-8 rounded-xl px-2 my-auto items-center" onClick={()=> dispatch(removeCart({name:cart.name}))}>Remove</button>
+            </div>
           </li>
         ))}
       </ul>
-
-      <div className="w-1/12 mx-auto mt-9">
-        <button className="w-8/12 h-8 mx-auto rounded-xl bg-sky-500 px-3 hover:scale-110 transition-all">Checkout</button>
-      </div>
+        <button className="w-1/12 h-8 mx-auto rounded-xl bg-sky-500 px-3 hover:scale-110 transition-all">Checkout</button>
     </div>
   );
 }
